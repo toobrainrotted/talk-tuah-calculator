bd1b200c5f08591cd8548c8a3399e99d6fcb37b9
toobrainrotted <toobrainrotted@gmail.com>
2024-10-22 16:48:21 +0200

    got rid of actions cruft

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/talk-tuah-calculator",
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
+  // basePath: "/talk-tuah-calculator",
+  // output: "export", // <=== enables static exports
+  // reactStrictMode: true,
 };
 
 export default nextConfig;
