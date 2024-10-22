728b76ac1cbaff1d3a5cff30759a1af7d058ca1a
toobrainrotted <toobrainrotted@gmail.com>
2024-10-22 16:40:09 +0200

    added github actions

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/talk-tuah-calculator",
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
